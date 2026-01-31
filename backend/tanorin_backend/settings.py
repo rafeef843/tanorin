6aebcad4d4233f197f7807ee723d3ee2a7aff3f9
Rafeef <rafeefmmz1@gmail.com>
2026-01-31 21:28:50 +0300

    Configure correct DB fallback to SQLite for build process

--- backend/tanorin_backend/settings.py
+++ backend/tanorin_backend/settings.py
@@ -97,15 +97,8 @@ DATABASES = {
 if not DATABASES["default"]:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.mysql",
-            "NAME": "tanorin_db",
-            "USER": "root",
-            "PASSWORD": "9867",
-            "HOST": "127.0.0.1",
-            "PORT": "3306",
-            "OPTIONS": {
-                "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-            },
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
 
