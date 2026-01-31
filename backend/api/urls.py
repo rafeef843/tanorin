15b73fd6a852c44ced02bc66a74c28dff505125a
Rafeef <rafeefmmz1@gmail.com>
2026-01-31 22:50:18 +0300

    Fix import error, update data dump, and enable auto-load

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, ProductViewSet, NewsArticleViewSet, ExhibitionViewSet, SiteContentViewSet, JobOpeningViewSet, JobApplicationViewSet, reset_admin_view
+from .views import CategoryViewSet, ProductViewSet, NewsArticleViewSet, ExhibitionViewSet, SiteContentViewSet, JobOpeningViewSet, JobApplicationViewSet
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
